92f7e794cdc8fa481953059f8acd350330ff6363
Suryaprakash31940 <124489852+Suryaprakash31940@users.noreply.github.com>
2023-02-05 14:24:17 +0530

    Add files via upload

---
+++ down.js
@@ -0,0 +1,398 @@
+function draw() {
+  //get the drawing canvas
+  let canvas = document.getElementById("rectangular-shape-outlinee");
+  //check if the context returns true
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+  }
+  let canvas = document.getElementById("rounded-rectanglee");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+ let canvas = document.getElementById("fontt");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("fontt");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("ellipse-outline-shape-variantt");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("rectangle");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("circle");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("cylinderss");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("diamondd");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("circle");
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+ let canvas = document.getElementById("parallelogramm");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("hexagon-geometrical-shape-outlinee")
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("cylinderrrr");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("dotted-linee");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("dotted-lines");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("diagonal-linee");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("double-arrowww");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("diagonal-arrowwww");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("minuss");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("minuss");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("right-arrowwww");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+let canvas = document.getElementById("left-arrowwww");
+ if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    //get the co ordinates
+    let X = canvas.width / 2;
+    let Y = canvas.height / 2;
+    //define the radius
+    let R = 45;
+    //begin drawing the circle
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //set the thickness of the draw print
+    ctx.lineWidth = 3;
+    //set the color of the draw print
+    ctx.strokeStyle = "#111";
+    //start drawing the object
+    ctx.stroke();
+}
+
